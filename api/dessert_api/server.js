5802226aebd53c0782be829226ac5310fd2bdd47
Pikalot <pikalot@hotmail.com>
2025-06-10 22:43:38 -0700

    Added server.js models and sources

---
+++ api/dessert_api/server.js
@@ -0,0 +1,16 @@
+const { SceHttpServer } = require('../util/SceHttpServer');
+
+function main() {
+
+        console.log('show server')
+
+    const API_ENDPOINTS = [
+        __dirname + '/routes/Dessert.js',
+    ];
+    const dessertServer = new SceHttpServer(API_ENDPOINTS, 8084, '/dessert_api/');
+    dessertServer.initializeEndpoints().then(() => {
+        dessertServer.openConnection();
+    });
+}
+
+main();
